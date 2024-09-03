69be7376c554e58807ebb12c50fb484f62e6c697
GOVIND DWIVEDI <govindofficial625@gmail.com>
2024-09-04 00:48:29 +0530

    added solutions

---
+++ D_1_Seating_Arrangements_easy_version.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros and constants
+#define pb push_back
+#define endl ("\n")
+#define pi (3.141592653589)
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini2(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, s, n) for (int i = n - 1; i >= s; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define scin(str) getline(cin, str)
+
+// Debugging Macros
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+
+// Aliases and Types
+using vec = vector<int>;
+using pii = pair<int, int>;
+using mapi = map<int, int>;
+using si = set<int>;
+
+const int mod = 1e9 + 7;
+
+// Global Constants
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1}; // for every grid problem!!
+const int N = 2e5 + 5;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    vec a(n * m);
+    rep(i, n * m)
+    {
+        cin >> a[i];
+    }
+    vec v(n * m);
+    rep(i, n * m)
+    {
+        v[i] = i;
+    }
+
+    // sort(all(a));
+
+    auto cmp = [&](int x, int y)
+    {
+        if(a[x] == a[y])
+        {
+            return x > y;
+        }
+        return a[x] < a[y];
+    };
+
+    sort(all(v), cmp);
+    rep(i, n * m)
+    {
+        v[i]++;
+    }
+    // rep(i, n*m){
+    // cout << v[i] << " ";
+    // }
+    int ans = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (v[i] > v[j])
+            {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
