62f053a0176285c0d67089f3b3a4dccee7ceac71
GOVIND DWIVEDI <govindofficial625@gmail.com>
2024-11-03 01:44:02 +0530

    added solutions

---
+++ B_Scale.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros and constants
+#define pb push_back
+#define endl ("\n")
+#define pi (3.141592653589)
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini2(a, b, c, d) min(d, min(c, min(a, b))) 
+#define rrep(i,s,n) for(int i=n-1;i>=s;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define scin(str) getline(cin, str)
+
+// Debugging Macros
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+
+// Aliases and Types
+using vec = vector<int>;
+using pii=pair<int,int>;
+using mapi=map<int,int>;
+using si=set<int>;
+
+const int mod = 1e9+7;
+
+// Global Constants
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const int N=2e5+5;
+
+
+void solve(){
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    vector<vector<char>> a(n, vector<char>(n, 0));
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i+=k){
+        for(int j=0;j<n;j+=k){
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+}
+
+
+int32_t main(){
+    fast
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        
+        
+        solve();
+
+
+    }
+    return 0;
+}
