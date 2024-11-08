de4754dd0e74325013fc5cf0e28a2c136d1df0c9
GOVIND DWIVEDI <govindofficial625@gmail.com>
2024-11-08 23:07:49 +0530

    added solutions

---
+++ B_Hungry_Sequence.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros and constants
+#define pb push_back
+#define endl ("\n")
+#define pi (3.141592653589)
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini2(a, b, c, d) min(d, min(c, min(a, b))) 
+#define rrep(i,s,n) for(int i=n-1;i>=s;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define scin(str) getline(cin, str)
+
+// Debugging Macros
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+
+// Aliases and Types
+using vec = vector<int>;
+using pii=pair<int,int>;
+using mapi=map<int,int>;
+using si=set<int>;
+
+const int mod = 1e9+7;
+
+// Global Constants
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const int N=2e7+5;
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime;
+}
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> ans;
+    vector<bool> s = sieve(N);
+    for(int i=0;i< s.size();i++) {
+        if(s[i]) {
+            ans.pb(i);
+        }
+    }
+
+    // cout << s.back();
+
+    for(int i=0;i<n;i++) {
+        cout << ans[i] << " ";
+    }
+
+}
+
+
+int32_t main(){
+    fast
+
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        
+        
+        solve();
+
+
+    }
+    return 0;
+}
