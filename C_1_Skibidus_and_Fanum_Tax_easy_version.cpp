9571daf51a27ce91519cc2b2305a4196668831e5
Govind Dwivedi <govindofficial625@gmail.com>
2025-06-28 00:09:00 +0530

    added solutions

---
+++ C_1_Skibidus_and_Fanum_Tax_easy_version.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros and constants
+#define endl ("\n")
+#define pi (3.141592653589)
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini2(a, b, c, d) min(d, min(c, min(a, b))) 
+#define rrep(i,s,n) for(int i=n-1;i>=s;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define scin(str) getline(cin, str)
+
+// Debugging Macros
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+
+// Aliases and Types
+using vec = vector<int>;
+using pii=pair<int,int>;
+using mapi=map<int,int>;
+using si=set<int>;
+
+const int mod = 1e9+7;
+
+// Global Constants
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const int N=2e5+5;
+
+
+void solve(){
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+    
+    int prev = -1;
+    bool possible = true;
+    for(int i = 0; i < n; i++) {
+        int v1 = a[i];
+        int v2 = b[0] - a[i];
+        int c1 = min(v1, v2);
+        int c2 = max(v1, v2);
+
+        if(c1 >= prev) {
+            prev = c1;
+        } 
+        else if(c2 >= prev) {
+            prev = c2;
+        } 
+        else {
+            possible = false;
+            break;
+        }
+    }
+
+    if(possible) {
+        cout << "YES" << endl;
+    } 
+    else {
+        cout << "NO" << endl;
+    }
+}
+
+
+int32_t main(){
+    fast
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        
+        
+        solve();
+
+
+    }
+    return 0;
+}
